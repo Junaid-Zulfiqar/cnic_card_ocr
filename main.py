f40091a86c9f164ac5e242aa1a69f3b2eb87222f
Junaid-Zulfiqar <junaidzulfiqar377@gmail.com>
2022-04-19 11:18:30 +0500

    update main file remove deskew function

--- main.py
+++ main.py
@@ -34,58 +34,6 @@ def save_img(image):
     data = Image.fromarray(image)
     data.save('pics/pic.jpg')
 
-def rotateImage(cvImage, angle: float):
-    newImage = cvImage.copy()
-    (h, w) = newImage.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return newImage
-
-def getSkewAngle(cvImage) -> float:
-    # Prep image, copy, convert to gray scale, blur, and threshold
-    newImage = cvImage.copy()
-    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (9, 9), 0)
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    # Apply dilate to merge text into meaningful lines/paragraphs.
-    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
-    # But use smaller kernel on Y axis to separate between different blocks of text
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
-    dilate = cv2.dilate(thresh, kernel, iterations=2)
-
-    # Find all contours
-    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)
-    for c in contours:
-        rect = cv2.boundingRect(c)
-        x,y,w,h = rect
-        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
-
-    # Find largest contour and surround in min area box
-    largestContour = contours[0]
-    print (len(contours))
-    minAreaRect = cv2.minAreaRect(largestContour)
-    cv2.imwrite('box.jpg', newImage)
-    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
-    angle = minAreaRect[-1]
-    if angle < -45:
-        angle = 90 + angle
-    return -1.0 * angle
-
-# Deskew image
-def deskew(cvImage):
-    angle = getSkewAngle(cvImage)
-    return rotateImage(cvImage, -2.0 * angle)
-
-def urdu_ocr(image):
-    url = 'http://167.172.31.248:7000/uploadfile'
-    files = {'file': open(f'{image}','rb')}
-    response = requests.post(url, files=files)
-    results = json.loads(response.text)
-    return results.get("output_text")
-
 def hsv(image):
     imgs = HSV(image)
     h_min = 34
@@ -123,8 +71,7 @@ def hsv_ocr_sections(section):
 
 
 def ocr_sections(section):
-    # section = cv2.cvtColor(section, cv2.COLOR_BGR2GRAY)
-    section = deskew(section)
+    section = cv2.cvtColor(section, cv2.COLOR_BGR2GRAY)
     custom_config = r'--oem 3 --psm 6'
     ocr_result = pytesseract.image_to_string(section, config=custom_config)
     ocr_result = ocr_result.replace("\n\f","")
